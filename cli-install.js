9dc1ca39f17454713d9ee9ccd86e97eded21dbc1
Bartek Kus <bartek@redmantech.com>
2019-11-14 19:41:30 -0700

    Remove cli-install / Fix MaxListenersExceededWarning by bumping the node version to 12.9.1

--- cli-install.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-
-require('events').EventEmitter.prototype._maxListeners = 0;
-
-const child_process = require('child_process');
-
-child_process.execSync("npm install",{stdio:[0,1,2]});
