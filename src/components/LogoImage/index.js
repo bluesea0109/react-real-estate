cf9b7036c3853e733e43274e7583b8ca42b3cff8
Bartek Kus <bartek@redmantech.com>
2019-11-15 09:33:03 -0700

    Adjust minor things

--- src/components/LogoImage/index.js
+++ src/components/LogoImage/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import { Image } from 'semantic-ui-react';
 
-export default () => <Image size="small" src={require('./logo_bm.svg')} />;
+export default () => <Image size="small" src={require('./logo_bm.svg')} alt="Brivity Marketer Logo" />;
--- src/services/Auth0.js
+++ src/services/Auth0.js
@@ -4,7 +4,6 @@ import config from '../config';
 const siteRoot = process.env.NODE_ENV === 'production' ? 'https://beta.brivitymarketer.com' : 'http://localhost:8082';
 
 const auth0Client = new auth0.WebAuth({
-  // the following three lines MUST be updated
   domain: config.auth0.domain,
   audience: `https://${config.auth0.domain}/userinfo`,
   clientID: config.auth0.clientId,
