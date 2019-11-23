9eb508e104bdd181478c8462df2b374d626cf194
Bartek Kus <bartek@redmantech.com>
2019-11-22 17:40:53 -0700

    Implement Navigation profile switch dropdown

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Dropdown } from 'semantic-ui-react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { NavigationLayout, MobileDisabledLayout, MobileEnabledLayout } from '../layouts';
 import { Link, useLocation } from 'react-router-dom';
-import { Menu } from './Base';
+import { Menu, Header } from './Base';
 
 const iconOnlyStyle = {
   margin: '0 auto 0 auto',
@@ -13,8 +16,14 @@ const iconWithTextStyle = {
   margin: '0 .5em 0 2.5em',
 };
 
+const StyledDropdown = styled(Dropdown)`
+  min-width: 8.3em !important;
+  max-width: 8.3em !important;
+`;
+
 export default () => {
   const [activeItem, setActiveItem] = useState('');
+  const [activeUser, setActiveUser] = useState('');
 
   let location = useLocation();
   React.useEffect(() => {
@@ -25,8 +34,57 @@ export default () => {
 
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
+  const isMultimode = useSelector(store => store.onLogin.mode === 'multiuser');
+  const loggedInUser = useSelector(store => store.onLogin.user);
+  const teammates = useSelector(store => store.team.profiles);
+  const profiles = [];
+
+  if (teammates.length > 0) {
+    teammates.map((profile, index) => {
+      return profiles.push({
+        key: index,
+        text: profile.last,
+        value: profile.userId,
+        content: <Header content={profile.first} subheader={profile.last} />,
+      });
+    });
+  }
+
+  useEffect(() => {
+    if (!activeUser && loggedInUser) {
+      setActiveUser(loggedInUser._id);
+    }
+  }, [loggedInUser, activeUser]);
+
+  const renderLabel = label => ({
+    color: 'blue',
+    content: `${label.text2}`,
+  });
+
+  const handleProfileSelect = (e, { value }) => {
+    console.log('profile value: ', value);
+
+    return setActiveUser(value);
+  };
+
   return (
     <NavigationLayout text>
+      {isMultimode && (
+        <Menu.Item style={{ marginLeft: '2.5em' }}>
+          <StyledDropdown
+            placeholder="Profile"
+            search
+            floating
+            scrolling
+            selection
+            options={profiles}
+            onChange={handleProfileSelect}
+            value={activeUser}
+            renderLabel={renderLabel}
+          />
+        </Menu.Item>
+      )}
+
       <Menu.Item as={Link} color="teal" name="dashboard" active={activeItem === '/dashboard'} onClick={handleItemClick} to="/dashboard">
         <MobileEnabledLayout style={iconOnlyStyle}>
           <FontAwesomeIcon icon="tachometer-alt" />
--- src/layouts/NavigationLayout.js
+++ src/layouts/NavigationLayout.js
@@ -3,14 +3,14 @@ import styled from 'styled-components';
 
 export default styled(Menu)`
   display: grid !important;
-  grid-template-rows: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
   grid-template-columns: 1fr;
 
   height: 160px;
 
   @media (max-width: 768px) {
     grid-template-rows: 1fr;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
 
     height: 60px;
     margin: 0 !important;
