c58b449792c3c523f4789ef3704822c2652e9f26
Bartek Kus <bartek@redmantech.com>
2019-11-25 17:37:39 -0700

    Add hover effect to dashboards preview images

--- src/components/MailoutListItem/ImageGroup.js
+++ src/components/MailoutListItem/ImageGroup.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 import { ItemBodyPicturesLayout } from '../../layouts';
 import { Image } from '../Base';
+import './hover.css';
 
 const mql = window.matchMedia('(max-width: 599px)');
 const resizePictures = () => (mql.matches ? { width: '100%' } : { width: '48%' });
@@ -15,10 +16,10 @@ const ImageGroup = ({ img1src, img2src, linkTo }) => {
     return (
       <ItemBodyPicturesLayout>
         <Link to={linkTo}>
-          <Image src={img1src} style={resizePictures()} />
+          <Image src={img1src} style={resizePictures()} className="bm-effect" />
         </Link>
         <Link to={linkTo}>
-          <Image src={img2src} style={resizePictures()} />
+          <Image src={img2src} style={resizePictures()} className="bm-effect" />
         </Link>
       </ItemBodyPicturesLayout>
     );
