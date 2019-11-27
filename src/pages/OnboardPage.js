76a38658c9c3807ef637a11862cb3e7416f9fa53
Bartek Kus <bartek@redmantech.com>
2019-11-27 13:57:07 -0700

    Implement onboarding page structure

--- src/pages/OnboardPage.js
+++ src/pages/OnboardPage.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
-import { Button, Page, Segment } from '../components/Base';
+import { Button, Header, Page, Segment } from '../components/Base';
 import { incrementStep, setOnboardedStatus } from '../store/modules/onboarded/actions';
 
 const OnboardPage = () => {
@@ -18,14 +18,38 @@ const OnboardPage = () => {
     }
   }, [step, dispatch, history]);
 
-  return (
-    <Page basic>
+  const renderFillInYourProfile = () => {
+    return (
       <Segment>
-        <h1>Onboarding Page</h1>
+        <Header as="h1">Profile</Header>
         <Button onClick={() => dispatch(incrementStep(1))}>Stage 1 Completed</Button>
+      </Segment>
+    );
+  };
+
+  const renderCustomize = () => {
+    return (
+      <Segment>
+        <Header as="h1">Customize</Header>
         <Button onClick={() => dispatch(incrementStep(2))}>Stage 2 Completed</Button>
+      </Segment>
+    );
+  };
+
+  const renderAutomationAndBilling = () => {
+    return (
+      <Segment>
+        <Header as="h1">Automation & Billing</Header>
         <Button onClick={() => dispatch(incrementStep(3))}>Stage 3 Completed</Button>
       </Segment>
+    );
+  };
+
+  return (
+    <Page basic>
+      {step === 0 && renderFillInYourProfile()}
+      {step === 1 && renderCustomize()}
+      {step === 2 && renderAutomationAndBilling()}
     </Page>
   );
 };
