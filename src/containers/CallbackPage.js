7ddff3d1b39bbd67bbc170d93e32e8186211a60a
Bartek Kus <bartek@redmantech.com>
2019-11-28 12:07:31 -0700

    Refactor to use hooks like the rest of the pages

--- src/containers/CallbackPage.js
+++ src/containers/CallbackPage.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Redirect } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { authenticate } from '../store/modules/auth0/actions';
 import Loading from '../components/Loading';
 
-const mapStateToProps = state => {
-  return {
-    auth0: state.auth0,
-  };
-};
+const CallbackPage = () => {
+  const dispatch = useDispatch();
+  const auth0 = useSelector(store => store.auth0);
 
-let Callback = ({ dispatch, auth0 }) => {
   if (auth0.authenticated) return <Redirect to="/dashboard" />;
 
   if (!auth0.pending) dispatch(authenticate());
 
   return <Loading />;
 };
-Callback = connect(mapStateToProps)(Callback);
 
-export default Callback;
+export default CallbackPage;
--- src/containers/IndexPage.js
+++ src/containers/IndexPage.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Redirect } from 'react-router';
+import { useSelector } from 'react-redux';
+
 import AuthService from '../services/auth';
 
-const mapStateToProps = state => {
-  return {
-    auth0: state.auth0,
-  };
-};
+const IndexPage = () => {
+  const auth0 = useSelector(store => store.auth0);
 
-let IndexPage = ({ auth0 }) => {
   if (auth0.authenticated) return <Redirect to="/dashboard" />;
 
   return <div>{AuthService.signIn()}</div>;
 };
-IndexPage = connect(mapStateToProps)(IndexPage);
 
 export default IndexPage;
