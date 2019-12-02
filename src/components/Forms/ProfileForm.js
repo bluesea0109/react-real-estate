d5edb293e0a654c990ef7f910b45c0e44aec35c7
Bartek Kus <bartek@redmantech.com>
2019-12-02 15:48:39 -0700

    Remove unneeded code

--- src/components/Forms/ProfileForm.js
+++ src/components/Forms/ProfileForm.js
@@ -177,8 +177,6 @@ const ProfileForm = () => {
                 </Button>
               </span>
             </div>
-
-            <pre>{JSON.stringify(values, 0, 2)}</pre>
           </Form>
         )}
       />
