4c98452062b31c0aa2eaa92b9fad62302600b24c
Bartek Kus <bartek@redmantech.com>
2019-12-16 16:01:24 -0700

    Fix iinviteTeamates action

--- src/store/modules/inviteUsers/actions.js
+++ src/store/modules/inviteUsers/actions.js
@@ -4,8 +4,8 @@ export const INVITE_USERS_PENDING = 'INVITE_USERS_PENDING';
 export const INVITE_USERS_SUCCESS = 'INVITE_USERS_SUCCESS';
 export const INVITE_USERS_ERROR = 'INVITE_USERS_ERROR';
 
-export function inviteUsersPending() {
-  return createAction(INVITE_USERS_PENDING);
+export function inviteUsersPending(payload) {
+  return createAction(INVITE_USERS_PENDING, payload);
 }
 
 export function inviteUsersSuccess(payload) {
