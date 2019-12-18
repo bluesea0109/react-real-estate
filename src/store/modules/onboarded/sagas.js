670b2f7935aeea3a5750d6e4d67fa8a49a9b7df5
Bartek Kus <bartek@redmantech.com>
2019-12-18 14:32:09 -0700

    Add functionality to onboarding saga to enable fill in profile submission to forward the onboarding progress

--- src/store/modules/onboarded/sagas.js
+++ src/store/modules/onboarded/sagas.js
@@ -2,9 +2,27 @@ import { put, select, takeLatest } from 'redux-saga/effects';
 
 import { setCompletedProfile, setCompletedTeamCustomization, setCompletedInviteTeammates } from './actions';
 import { GET_ON_LOGIN_SUCCESS } from '../onLogin/actions';
+import { SAVE_TEAM_PROFILE_SUCCESS } from '../teamProfile/actions';
+import { SAVE_PROFILE_SUCCESS } from '../profile/actions';
 
 export const getSetupComplete = state => state.onLogin && state.onLogin.userProfile;
 export const checkIfUserIsAdmin = state => state.onLogin && state.onLogin.permissions && state.onLogin.permissions.teamAdmin;
+
+export const getUserProfile = state => state.profile && state.profile.available;
+export const getTeamProfile = state => state.teamProfile && state.teamProfile.available;
+
+export function* onboardedCompleteProfileSetupSaga() {
+  try {
+    const userProfileIsAvailable = yield select(getUserProfile);
+    const teamProfileIsAvailable = yield select(getTeamProfile);
+
+    if (userProfileIsAvailable && teamProfileIsAvailable) {
+      yield put(setCompletedProfile(true));
+    }
+  } catch (err) {
+    console.log('completeProfileSetupSaga err', err);
+  }
+}
 
 export function* onboardedSaga() {
   try {
@@ -26,4 +44,6 @@ export function* onboardedSaga() {
 
 export default function*() {
   yield takeLatest(GET_ON_LOGIN_SUCCESS, onboardedSaga);
+  yield takeLatest(SAVE_TEAM_PROFILE_SUCCESS, onboardedCompleteProfileSetupSaga);
+  yield takeLatest(SAVE_PROFILE_SUCCESS, onboardedCompleteProfileSetupSaga);
 }
