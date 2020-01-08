2b44db9fc9f6003aec89cc865a0d2968b9d50d09
Bartek Kus <bartek@redmantech.com>
2020-01-08 13:03:41 -0700

    "Add commit npm script"

--- utils/commit.js
+++ utils/commit.js
@@ -3,26 +3,44 @@
 
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const spawn = util.promisify(require('child_process').spawn);
+
+async function version(versionType) {
+  const { stdout, stderr } = await exec(`npm version ${versionType} --no-git-tag-version --force`);
+  if (stderr) throw stderr;
+  return stdout;
+}
+
+async function add() {
+  const { stderr } = await spawn('git', ['add', 'package.json'], { stdio: 'inherit' });
+  if (stderr) throw stderr;
+}
+
+async function commit(gitMessage) {
+  const { stderr } = await spawn('git', ['commit', '-m', `"${gitMessage}"`], { stdio: 'inherit' });
+  if (stderr) throw stderr;
+}
+
+async function tag(npmVersion) {
+  const { stderr } = await spawn('git', ['tag', `${npmVersion}`], { stdio: 'inherit' });
+  if (stderr) throw stderr;
+}
+
+async function status() {
+  const { stderr } = await spawn('git', ['status'], { stdio: 'inherit' });
+  if (stderr) throw stderr;
+}
 
 const run = async () => {
   try {
     const versionType = process.argv[2];
     const gitMessage = process.argv[3];
 
-    let { stdout, stderr } = await exec(`npm version ${versionType} --no-git-tag-version --force`);
-    if (stderr) throw stderr;
-
-    const response = await exec(`git commit -m "${gitMessage}"`);
-
-    console.log('response', response);
-
-    const response1 = await exec(`git tag ${stdout}`);
-
-    console.log('response1', response1);
-
-    const response2 = await exec(`git status`);
-
-    console.log('response2', response2);
+    const npmVersion = await version(versionType);
+    await add();
+    await commit(gitMessage);
+    await tag(npmVersion);
+    await status();
 
   } catch (err) {
     console.log('Something went wrong:');
