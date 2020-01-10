007ab3074f04620612f30b7fa3ca13899ff1e1c0
Bartek Kus <bartek@redmantech.com>
2020-01-10 12:00:37 -0700

    Fix commit script

--- utils/commit.js
+++ utils/commit.js
@@ -16,6 +16,9 @@ const run = async () => {
     const versionType = process.argv[2];
     const gitMessage = process.argv[3];
 
+    if (versionType !== 'patch' && versionType !== 'minor' && versionType !== 'major') throw new Error('You need to specify npm version! [patch|minor|major]');
+    if (!gitMessage) throw new Error('You need to provide a git commit message!');
+
     const npmVersion = await version(versionType);
     await spawn('git', ['add', 'package.json', 'package-lock.json'], { stdio: 'inherit' });
     await spawn('git', ['commit', '-m', gitMessage.trim()], { stdio: 'inherit' });
@@ -24,7 +27,8 @@ const run = async () => {
 
   } catch (err) {
     console.log('Something went wrong:');
-    console.error(err);
+    console.error(err.message);
+    console.error('\nPlease use this format: \nnpm run commit [patch|minor|major] "Commit message"');
   }
 };
 
