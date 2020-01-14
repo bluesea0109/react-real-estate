f70641db65b0b40039616c74fc5f85be78a60dbd
Bartek Kus <bartek@redmantech.com>
2020-01-13 18:56:00 -0700

    Implement postcard image retry based on the rendered size of the images returned from the api

--- src/store/modules/postcards/sagas.js
+++ src/store/modules/postcards/sagas.js
@@ -1,19 +1,63 @@
-import { put, call, takeLatest } from 'redux-saga/effects';
+import { call, delay, put, race, take } from 'redux-saga/effects';
 
-import { GENERATE_POSTCARDS_PREVIEW_PENDING, generatePostcardsPreviewSuccess, generatePostcardsPreviewError } from './actions';
+import {
+  GENERATE_POSTCARDS_PREVIEW_PENDING,
+  GENERATE_POSTCARDS_PREVIEW_SUCCESS,
+  generatePostcardsPreviewSuccess,
+  generatePostcardsPreviewError,
+} from './actions';
 import ApiService from '../../../services/api/index';
 
-export function* getPostcardsPreviewSaga() {
-  try {
-    const { path, method } = ApiService.directory.onboard.customization.generatePostcardPreview();
-    const response = yield call(ApiService[method], path);
+const getDimensions = ({ url }) => {
+  return new Promise((resolve, reject) => {
+    let img = new Image();
+    img.onload = () => resolve({ height: img.height, width: img.width, url });
+    img.onerror = reject;
+    img.src = url;
+  });
+};
 
-    yield put(generatePostcardsPreviewSuccess(response));
-  } catch (err) {
-    yield put(generatePostcardsPreviewError(err.message));
+export function* getPostcardsPreviewSaga() {
+  while (true) {
+    try {
+      const { path, method } = ApiService.directory.onboard.customization.generatePostcardPreview();
+      const response = yield call(ApiService[method], path);
+
+      const listedFrontLarge = yield getDimensions({ url: response.listed.sampleFrontLargeUrl });
+      const listedBackLarge = yield getDimensions({ url: response.listed.sampleBackLargeUrl });
+      const soldFrontLarge = yield getDimensions({ url: response.sold.sampleFrontLargeUrl });
+      const soldBackLarge = yield getDimensions({ url: response.sold.sampleBackLargeUrl });
+
+      if (
+        listedFrontLarge.height > 0 &&
+        listedFrontLarge.width > 0 &&
+        listedBackLarge.height > 0 &&
+        listedBackLarge.width > 0 &&
+        soldFrontLarge.height > 0 &&
+        soldFrontLarge.width > 0 &&
+        soldBackLarge.height > 0 &&
+        soldBackLarge.width > 0
+      ) {
+        yield put(generatePostcardsPreviewSuccess(response));
+      } else {
+        console.log('listedFrontLarge', listedFrontLarge);
+        console.log('listedBackLarge', listedBackLarge);
+        console.log('soldFrontLarge', soldFrontLarge);
+        console.log('soldBackLarge', soldBackLarge);
+
+        yield delay(1000);
+      }
+    } catch (err) {
+      console.log('getPostcardsPreviewSaga');
+      console.error(err);
+      yield put(generatePostcardsPreviewError(err.message));
+    }
   }
 }
 
 export default function*() {
-  yield takeLatest(GENERATE_POSTCARDS_PREVIEW_PENDING, getPostcardsPreviewSaga);
+  while (true) {
+    yield take(GENERATE_POSTCARDS_PREVIEW_PENDING);
+    yield race([call(getPostcardsPreviewSaga), take(GENERATE_POSTCARDS_PREVIEW_SUCCESS)]);
+  }
 }
--- src/store/modules/teamPostcards/sagas.js
+++ src/store/modules/teamPostcards/sagas.js
@@ -1,19 +1,63 @@
-import { put, call, takeLatest } from 'redux-saga/effects';
+import { call, delay, put, race, take } from 'redux-saga/effects';
 
-import { GENERATE_TEAM_POSTCARDS_PREVIEW_PENDING, generateTeamPostcardsPreviewSuccess, generateTeamPostcardsPreviewError } from './actions';
+import {
+  GENERATE_TEAM_POSTCARDS_PREVIEW_PENDING,
+  GENERATE_TEAM_POSTCARDS_PREVIEW_SUCCESS,
+  generateTeamPostcardsPreviewError,
+  generateTeamPostcardsPreviewSuccess,
+} from './actions';
 import ApiService from '../../../services/api/index';
 
-export function* getTeamPostcardsPreviewSaga() {
-  try {
-    const { path, method } = ApiService.directory.onboard.teamCustomization.generatePostcardPreview();
-    const response = yield call(ApiService[method], path);
+const getDimensions = ({ url }) => {
+  return new Promise((resolve, reject) => {
+    let img = new Image();
+    img.onload = () => resolve({ height: img.height, width: img.width, url });
+    img.onerror = reject;
+    img.src = url;
+  });
+};
 
-    yield put(generateTeamPostcardsPreviewSuccess(response));
-  } catch (err) {
-    yield put(generateTeamPostcardsPreviewError(err.message));
+export function* getTeamPostcardsPreviewSaga() {
+  while (true) {
+    try {
+      const { path, method } = ApiService.directory.onboard.teamCustomization.generatePostcardPreview();
+      const response = yield call(ApiService[method], path);
+
+      const listedFrontLarge = yield getDimensions({ url: response.listed.sampleFrontLargeUrl });
+      const listedBackLarge = yield getDimensions({ url: response.listed.sampleBackLargeUrl });
+      const soldFrontLarge = yield getDimensions({ url: response.sold.sampleFrontLargeUrl });
+      const soldBackLarge = yield getDimensions({ url: response.sold.sampleBackLargeUrl });
+
+      if (
+        listedFrontLarge.height > 0 &&
+        listedFrontLarge.width > 0 &&
+        listedBackLarge.height > 0 &&
+        listedBackLarge.width > 0 &&
+        soldFrontLarge.height > 0 &&
+        soldFrontLarge.width > 0 &&
+        soldBackLarge.height > 0 &&
+        soldBackLarge.width > 0
+      ) {
+        yield put(generateTeamPostcardsPreviewSuccess(response));
+      } else {
+        console.log('listedFrontLarge', listedFrontLarge);
+        console.log('listedBackLarge', listedBackLarge);
+        console.log('soldFrontLarge', soldFrontLarge);
+        console.log('soldBackLarge', soldBackLarge);
+
+        yield delay(1000);
+      }
+    } catch (err) {
+      console.log('getTeamPostcardsPreviewSaga');
+      console.error(err);
+      yield put(generateTeamPostcardsPreviewError(err.message));
+    }
   }
 }
 
 export default function*() {
-  yield takeLatest(GENERATE_TEAM_POSTCARDS_PREVIEW_PENDING, getTeamPostcardsPreviewSaga);
+  while (true) {
+    yield take(GENERATE_TEAM_POSTCARDS_PREVIEW_PENDING);
+    yield race([call(getTeamPostcardsPreviewSaga), take(GENERATE_TEAM_POSTCARDS_PREVIEW_SUCCESS)]);
+  }
 }
