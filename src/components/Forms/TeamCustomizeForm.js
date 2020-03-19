295665bc1fcb750c4866252acc5e5c75a3218e7d
Bartek Kus <bartek@redmantech.com>
2020-03-19 13:50:01 -0600

    Fix issue #288 | Fix issue #289

--- src/components/Forms/TeamCustomizeForm.js
+++ src/components/Forms/TeamCustomizeForm.js
@@ -99,13 +99,20 @@ const CustomizeForm = ({ teamCustomizationData, initialValues }) => {
   const triggerSubmit = () => {
     if (formRef.current && !formRef.current.status) {
       formRef.current.handleSubmit();
+    } else {
+      if (page === 1 && !!formRef.current.status?.sold_cta) {
+        setPage(page + 1);
+      }
+      if (page === 2 && !!formRef.current.status?.listed_cta) {
+        setPage(page - 1);
+      }
     }
   };
 
   const nextPage = () => {
     if (formRef.current) {
       formRef.current.validateForm().then(errors => {
-        if (objectIsEmpty(errors) && !formRef.current.status) {
+        if ((objectIsEmpty(errors) && !formRef.current.status) || (page === 1 && !!formRef.current.status?.sold_cta)) {
           setPage(page + 1);
         } else if (!formRef.current.status) {
           formRef.current.handleSubmit();
@@ -117,7 +124,7 @@ const CustomizeForm = ({ teamCustomizationData, initialValues }) => {
   const prevPage = () => {
     if (formRef.current) {
       formRef.current.validateForm().then(errors => {
-        if (objectIsEmpty(errors) && !formRef.current.status) {
+        if ((objectIsEmpty(errors) && !formRef.current.status) || (page === 2 && !!formRef.current.status?.listed_cta)) {
           setPage(page - 1);
         } else if (!formRef.current.status) {
           formRef.current.handleSubmit();
