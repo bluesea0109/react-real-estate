cf9ce2a9d408c1e467dca9716d30f8f80073e928
Ryan Ramage <ryan.ramage@gmail.com>
2020-06-25 12:20:51 -0600

    fetch billings

--- src/pages/BillingPage.js
+++ src/pages/BillingPage.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Loading from '../components/Loading';
 import { Button, Header, Icon, Menu, Message, Modal, Page, Segment } from '../components/Base';
+import { Table } from 'semantic-ui-react';
+
 import InviteTeammatesForm from '../components/Forms/InviteTeammatesForm';
 import { passwordReset } from '../store/modules/auth0/actions';
 import PageTitleHeader from '../components/PageTitleHeader';
 import { ContentBottomHeaderLayout, ContentSpacerLayout, ContentTopHeaderLayout, ItemHeaderLayout, ItemHeaderMenuLayout } from '../layouts';
 import { isMobile } from '../components/utils';
+import auth from '../services/auth';
+import api from '../services/api';
+
 
 const BillingPage = () => {
   const dispatch = useDispatch();
@@ -15,6 +20,24 @@ const BillingPage = () => {
   const multiUser = onLoginMode === 'multiuser';
   const [billingDetails, setBillingDetails] = useState(null)
 
+  useEffect(() => {
+    async function fetchData () {
+      if (billingDetails) return
+      let path = `/api/user/team/settings/billings`
+      const headers = {};
+      const accessToken = await auth.getAccessToken();
+      headers['authorization'] = `Bearer ${accessToken}`;
+      const response = await fetch(path, { headers, method: 'get', credentials: 'include' });
+      const results = await api.handleResponse(response)
+
+      console.log(results)
+      setBillingDetails(results)
+    }
+    fetchData()
+  }, [billingDetails])
+
+
+  console.log(billingDetails)
   return (
     <Page basic>
       <ContentTopHeaderLayout>
@@ -38,9 +61,27 @@ const BillingPage = () => {
               {!billingDetails && <Loading message="Retriving billing information..." />}
             </ContentBottomHeaderLayout>
             {billingDetails && (
-              <Header as="h4" style={{ marginLeft: '1.5em', marginBottom: '-0.5em' }}>
-                Billing Details
-              </Header>
+              <Table celled>
+                 <Table.Header>
+                   <Table.Row>
+                     <Table.HeaderCell>Date</Table.HeaderCell>
+                     <Table.HeaderCell>Campaign Title</Table.HeaderCell>
+                     <Table.HeaderCell>Postcards Sent</Table.HeaderCell>
+                     <Table.HeaderCell>Credits Used</Table.HeaderCell>
+                     <Table.HeaderCell>Total Amount</Table.HeaderCell>
+                   </Table.Row>
+                 </Table.Header>
+
+                 <Table.Body>
+                   <Table.Row>
+                     <Table.Cell>March 21, 2020</Table.Cell>
+                     <Table.Cell>123 Link Road</Table.Cell>
+                     <Table.Cell>180</Table.Cell>
+                     <Table.Cell>0</Table.Cell>
+                     <Table.Cell>$128.27</Table.Cell>
+                   </Table.Row>
+                 </Table.Body>
+                </Table>
             )}
 
           </Segment>
