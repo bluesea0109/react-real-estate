e43728fddc3b125061ae38d41d08204f69efe72a
Derryk Boyd <derrykboyd@gmail.com>
2020-10-07 08:57:36 -0600

    Update main grid row (Fixes #439)

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -4,14 +4,14 @@ export default styled.div`
   height: 100vh;
 
   display: grid;
-  grid-template-rows: [header] 60px [body] 110vh;
+  grid-template-rows: [header] 60px [body] 1fr;
   grid-template-columns: [sidebar] 175px [content] 1fr;
   grid-template-areas:
     'header header'
     'sidebar content';
 
   @media (max-width: 768px) {
-    grid-template-rows: [header] 60px [sidebar] 60px [body] 110vh;
+    grid-template-rows: [header] 60px [sidebar] 60px [body] 1fr;
     grid-template-columns: [content] 1fr;
     grid-template-areas:
       'header'
