803041156a08c6abc8070e8cbac7319650e3deab
bluesea0109 <ub2099236@gmail.com>
2020-11-23 14:38:29 -0700

    Reset scroll to top when navigating to a new page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { MainLayout, HeaderLayout, SidebarLayout, ContentLayout } from './layouts';
 import TopBarContainer from './containers/TopBarContainer';
 import Navigation from './components/Navigation';
 import Router from './Router';
+import { useLocation } from 'react-router';
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <MainLayout>
       <HeaderLayout>
