3bdbd96154c779c759705909d2862072fdb57aab
bluesea0109 <ub2099236@gmail.com>
2020-12-01 18:23:52 -0700

    Fix formatting

--- src/components/__tests__/Hooks/useIsMobile.test.js
+++ src/components/__tests__/Hooks/useIsMobile.test.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { renderHook } from '@testing-library/react-hooks';
 import { render, fireEvent } from '@testing-library/react';
 import { useIsMobile } from '../../Hooks/useIsMobile';
@@ -6,19 +6,19 @@ import { useIsMobile } from '../../Hooks/useIsMobile';
 const TestComponent = () => {
   const isMobile = useIsMobile();
   return <>{`${isMobile}`}</>;
-}
+};
 
 it('returns false for window width > 600px', () => {
   global.innerWidth = 620;
   const { result } = renderHook(() => useIsMobile());
   expect(result.current).toBe(false);
-})
+});
 
 it('returns true for window width < 600px', () => {
   global.innerWidth = 580;
   const { result } = renderHook(() => useIsMobile());
   expect(result.current).toBe(true);
-})
+});
 
 it('updates on page resize', () => {
   const { container } = render(<TestComponent />);
@@ -26,4 +26,4 @@ it('updates on page resize', () => {
   global.innerWidth = 620;
   fireEvent(global, new Event('resize'));
   expect(container.innerHTML).toEqual('false');
-})
+});
