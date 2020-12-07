f56bb8461f9c3b0e96240d37b672b9420373a596
Eduardo Martinez <dwardmr@gmail.com>
2020-12-07 15:42:43 -0600

    Josiah's pre release feedback

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,9 +3,9 @@ import { Header } from 'semantic-ui-react';
 
 import { Card, Image, Segment } from './Base';
 
-const Loading = ({ message = 'Loading, please wait...' }) => (
+const Loading = ({ message = 'Loading, please wait...', whiteBg }) => (
   <Segment basic style={{ margin: '6em auto' }}>
-    <Card centered style={{ backgroundColor: '#f9f8f7', minWidth: '380px', boxShadow: 'none' }}>
+    <Card centered style={{ backgroundColor: whiteBg ? '#fff' : '#f9f8f7', minWidth: '380px', boxShadow: 'none' }}>
       <Image centered size="tiny" src={require('../assets/loading.gif')} style={{ background: 'unset', marginTop: '1em' }} />
       <Card.Content style={{ borderTop: 'none' }}>
         <Header as="h2" textAlign="center">
