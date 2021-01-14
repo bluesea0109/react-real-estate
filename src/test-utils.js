62170f2176f0212abae7dfe09a3b0eca54aa1756
bluesea0109 <ub2099236@gmail.com>
2021-01-14 16:17:31 -0700

    Change custom back buttons to consistent links
    Remove unused react-last-location dependency

--- src/test-utils.js
+++ src/test-utils.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { render as rtlRender } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { LastLocationProvider } from 'react-router-last-location';
 import PolygonGoogleMapsHOC from './components/Forms/PolygonGoogleMaps/PolygonGoogleMapsHOC';
 import AuthService from './services/auth';
 import configureStore from './store/configure';
@@ -17,9 +16,7 @@ export function render(
     return (
       <Provider store={store}>
         <BrowserRouter basename={process.env.PUBLIC_URL}>
-          <LastLocationProvider>
-            <PolygonGoogleMapsHOC>{children}</PolygonGoogleMapsHOC>
-          </LastLocationProvider>
+          <PolygonGoogleMapsHOC>{children}</PolygonGoogleMapsHOC>
         </BrowserRouter>
       </Provider>
     );
