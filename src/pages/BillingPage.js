0474421b5532b0dbbfa97945eab4ff36e81b0708
Sean Connelly <seanconnelly@Seans-MacBook-Pro.local>
2021-02-22 15:29:49 -0500

    added billing info to top of page

--- src/pages/BillingPage.js
+++ src/pages/BillingPage.js
@@ -10,6 +10,7 @@ import { ContentBottomHeaderLayout, ContentTopHeaderLayout } from '../layouts';
 import auth from '../services/auth';
 import api from '../services/api';
 import { postcardDimensionsDisplayed } from '../components/utils/utils';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const BillingPage = () => {
   const isAdmin = useSelector(store => store.onLogin?.permissions?.teamAdmin);
@@ -20,6 +21,56 @@ const BillingPage = () => {
     store => store.onLogin?.userProfile.brivitySync.billing_reference
   );
   const [billingDetails, setBillingDetails] = useState(null);
+
+  let BillingInfo = null;
+  let billingName = null;
+  let billingAddress1 = null;
+  let billingAddress2 = null;
+  let maskedCardXCount = null;
+  let maskedCardNumbers = null;
+  let displayCard = null;
+  if (billingDetails && billingDetails.chargify.paymentProfile) {
+    const {
+      first_name,
+      last_name,
+      masked_card_number,
+      billing_address,
+      billing_city,
+      billing_state,
+      billing_zip,
+    } = billingDetails.chargify?.paymentProfile;
+
+    maskedCardXCount = masked_card_number.match(/X/g);
+    maskedCardNumbers = masked_card_number.match(/\d+/g).map(Number);
+
+    displayCard = maskedCardXCount.map((x, index) => {
+      if ((index + 1) % 4 !== 0) return <span key={index}>&middot;</span>;
+      else return <span key={index}>&middot; &nbsp;</span>;
+    });
+
+    billingName = (
+      <span>
+        {first_name} {last_name}
+      </span>
+    );
+
+    billingAddress2 = (
+      <span>
+        {billing_city} {billing_state} {billing_zip}
+      </span>
+    );
+    billingAddress1 = billing_address;
+  }
+  if (billingDetails && billingDetails.chargify.hasOwnProperty('error')) {
+    BillingInfo = (
+      <span>
+        Your credit card is not configured.{' '}
+        <a href="https://help.chargify.com/settings/self-service-page-urls.html" target="_blank">
+          Please contact support to set this up.
+        </a>
+      </span>
+    );
+  }
 
   useEffect(() => {
     async function fetchData() {
@@ -83,6 +134,57 @@ const BillingPage = () => {
             </p>
           )}
         </PageTitleHeader>
+        <div style={{ margin: '20px 0' }}>
+          <Segment style={{ padding: '20px' }}>
+            <Header as="h2">Card Details</Header>
+            {billingDetails && billingDetails.chargify.paymentProfile ? (
+              <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                <div style={{ marginRight: '120px', marginTop: '20px' }}>
+                  <p>
+                    <span style={{ fontWeight: 'bold' }}>Card holder Name </span>
+                  </p>
+                  <p style={{ marginTop: '15px' }}>{billingName}</p>
+                </div>
+                <div style={{ marginRight: '120px', marginTop: '20px' }}>
+                  <p style={{ marginBottom: '11px' }}>
+                    <span style={{ fontWeight: 'bold' }}>Card Number </span>
+                  </p>
+                  <div style={{ float: 'left' }}>
+                    <FontAwesomeIcon
+                      icon="credit-card"
+                      style={{ marginRight: '10px', marginTop: '6px', marginLeft: '3px' }}
+                    />
+                  </div>
+                  <div style={{ float: 'left' }}>
+                    {/* have to put this code above and then insert */}
+                    <p style={{ fontSize: '20px' }}>{displayCard}</p>
+                  </div>
+                  <div style={{ float: 'left', paddingTop: '5px' }}>
+                    <p style={{ fontSize: '12px' }}>
+                      <span>{maskedCardNumbers}</span>
+                    </p>
+                  </div>
+                </div>
+                <div style={{ marginRight: '120px', marginTop: '20px' }}>
+                  <p>
+                    <span style={{ fontWeight: 'bold' }}>Address </span>
+                  </p>
+                  <p style={{ marginTop: '15px' }}>
+                    {billingAddress1}
+                    <br />
+                    {billingAddress2}
+                  </p>
+                </div>
+              </div>
+            ) : (
+              <div style={{ marginRight: '120px', marginTop: '20px' }}>
+                <p>
+                  <span style={{ fontWeight: 'bold' }}>{BillingInfo}</span>
+                </p>
+              </div>
+            )}
+          </Segment>
+        </div>
       </ContentTopHeaderLayout>
       <div style={{ margin: '20px 0' }}>
         <Segment>
@@ -132,7 +234,7 @@ const BillingPage = () => {
           <Segment>
             <ContentBottomHeaderLayout style={{ minHeight: 0 }}>
               <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-                <p style={{ marginRight: '40px' }}>
+                <p style={{ marginRight: '100px' }}>
                   <span style={{ fontWeight: 'bold' }}>Credit Balance: </span> $
                   {billingDetails.credits}
                 </p>
