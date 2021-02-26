baff6ea3499d9221180444d6a5fe570d7194ebc8
bluesea0109 <ub2099236@gmail.com>
2021-02-26 16:11:54 -0700

    Refactor MailoutDetailsPage

---
+++ src/pages/MailoutDetailsPage/index.js
@@ -0,0 +1,13 @@
+export { default as MailoutDetailsPage } from './MailoutDetailsPage';
+export { default as TopHeader } from './TopHeader';
+export { default as FrontIframe } from './FrontIframe';
+export { default as BackIframe } from './BackIframe';
+export { default as RenderRecipients } from './RenderRecipients';
+export { default as MailoutData } from './MailoutData';
+
+export const IFrameSegStyle = {
+  border: 'none',
+  boxShadow: 'none',
+  padding: '0',
+  margin: 'auto',
+};
