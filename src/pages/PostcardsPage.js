db897e11bac0a7ad2dfbf8b95f065473cdeb53a5
logan <logan@redmantech.com>
2021-03-05 09:54:10 -0500

    fix postcards filter menu

--- src/pages/PostcardsPage.js
+++ src/pages/PostcardsPage.js
@@ -57,6 +57,9 @@ const StyledDropdown = styled(Dropdown)`
     font-weight: normal;
     text-transform: capitalize;
   }
+  &&&.ui.selection.dropdown {
+    min-width: 15em !important;
+  }
 `;
 
 const ModalWelcome = styled(Modal)`
